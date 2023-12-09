2e1e67dfe48b8aeff06809468547dd6f2935edf6
Gemma Ramos Casado <gramosc@enaire.es>
2023-12-09 17:10:00 +0100

    Ampliación microservicios

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -1,6 +1,7 @@
 import http.client
 import os
 import unittest
+from urllib.error import HTTPError
 from urllib.request import urlopen
 
 import pytest
@@ -8,6 +9,7 @@ import pytest
 BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
 DEFAULT_TIMEOUT = 2  # in secs
+
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
@@ -34,6 +36,42 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "15", "ERROR ADD"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/6/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1.5", "ERROR ADD"
+        )
+
+    def test_api_divide_by_zero(self):
+        url = f"{BASE_URL}/calc/divide/5/0"  # Intentando dividir por 0
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertNotEqual(
+                response.status, http.client.OK, f"Se esperaba un error en {url}"
+            )
+            self.assertEqual(
+                response.status, 406, f"Se esperaba un error HTTP 406 en {url}"
+            )
+        except HTTPError as e:
+            self.assertEqual(
+                e.code, 406, f"Se esperaba un error HTTP 406 en {url}"
+            )
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
